0ab099fedf862cf29d788bb7b203a62f1e0b3733
Oleksandr Zoria <oleksandr.zoria@p2h.com>
2021-10-06 20:40:04 +0300

    implemented main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Layout from "./components/Layout/Layout";
 import { observer } from 'mobx-react';
-import Button from '@mui/material/Button';
-import { useStores } from '../src/hooks/use-stores';
 
 function App() {
-  const { testStore } = useStores();
-  const { start, current, elapsedTime, tick } = testStore;
-  
-  const handleSubmit = () => {
-    console.log('current before', current);
-    tick()
-    console.log('current after', current);
-  }
   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>Start {start}</p>
-        <p>Current {current}</p>
-        <p>Elapsed time {elapsedTime}</p>
-        <Button variant="contained" onClick={handleSubmit}>Tick</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <Layout />
     </div>
   );
 }
