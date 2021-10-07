4e7a6ffd652e79c73fb171316b86129a7247b451
Oleksandr Zoria <oleksandr.zoria@p2h.com>
2021-10-07 20:34:30 +0300

    implemented routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import './App.scss';
 import Layout from "./components/Layout/Layout";
+import Home from './views/Home'
+import Projects from './views/Projects'
 import { observer } from 'mobx-react';
+import { Route, Switch, Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history'
 
 function App() {
+  const newHistory = createBrowserHistory();
   
   return (
     <div className="root">
-      <Layout />
+      <Router history={newHistory}>
+        <Layout>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              component={Home}
+            />
+            <Route
+              exact
+              path="/projects"
+              component={Projects}
+            />
+          </Switch>
+        </Layout>
+      </Router>
     </div>
   );
 }
